ffdf4872eabe5002e7edacb6943bc2cf16f08599
Gaspar <e972136@gmail.com>
2022-12-06 15:12:16 -0600

    Original

---
+++ src/main/java/com/codecademy/plants2/Plants2Application.java
@@ -0,0 +1,13 @@
+package com.codecademy.plants2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Plants2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Plants2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecademy/plants2/Plants2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecademy.plants2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Plants2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
