c35366b6b58a28500e6a200bef4f13382ca4329e
Gaspar <e972136@gmail.com>
2022-12-06 17:24:44 -0600

    terminado hasta el 11

--- src/main/java/com/codecademy/plants2/controllers/PlantController.java
+++ src/main/java/com/codecademy/plants2/controllers/PlantController.java
@@ -2,8 +2,9 @@ package com.codecademy.plants2.controllers;
 
 import com.codecademy.plants2.entities.Plant;
 import com.codecademy.plants2.repositories.PlantRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class PlantController {
@@ -16,5 +17,51 @@ public class PlantController {
     @GetMapping("/plants")
     public Iterable<Plant>  getAllPlants(){
         return plantRepository.findAll();
+    }
+
+    @GetMapping("/plants/{id}")
+    public Optional<Plant> getPlantById(@PathVariable Integer id){
+        return plantRepository.findById(id);
+    }
+
+    @PostMapping("/plants")
+    public Plant createNewPlant(@RequestBody Plant plant){
+        Plant save = this.plantRepository.save(plant);
+        return save;
+    }
+
+    @PutMapping("/plants/{id}")
+    public Plant updatePlant(@PathVariable  Integer id, @RequestBody Plant plant){
+        Optional<Plant> byId = this.plantRepository.findById(id);
+        if(!byId.isPresent()){
+            return null;
+        }
+        Plant plantToUpdate = byId.get();
+        if(plant.getName()!=null)
+            plantToUpdate.setName(plant.getName());
+
+        if(plant.getQuantity()!=null)
+            plantToUpdate.setQuantity(plant.getQuantity());
+
+        if(plant.getHasFruit()!=null)
+            plantToUpdate.setHasFruit(plant.getHasFruit());
+
+        if(plant.getWateringFrequency()!=null)
+            plantToUpdate.setWateringFrequency(plant.getWateringFrequency());
+
+        Plant updatedPlant = this.plantRepository.save(plantToUpdate);
+        return updatedPlant;
+    }
+
+    @DeleteMapping("/plants/{id}")
+    public Plant deletePlant(@PathVariable("id") Integer id){
+        Optional<Plant> plantToDeleteOptional = this.plantRepository.findById(id);
+        if(!plantToDeleteOptional.isPresent()){
+            return null;
+        }
+        Plant plantToDelete = plantToDeleteOptional.get();
+        this.plantRepository.deleteById(id);
+        return plantToDelete;
+
     }
 }
